9bb58aee469c703b75c0aa449b4bfd0f1703a23f
Steffen Koch <steffen543@live.de>
2018-03-15 17:32:34 +0100

    Initial Commit

---
+++ src/Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public float Speed = 100f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward, Speed * Time.deltaTime);
+	}
+}
---
+++ src/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private float points = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddPoints(float points)
+    {
+        this.points += points;
+        Debug.Log("Gesamte Punkte: " + this.points);
+    }
+}
